9e044088de85f5c10c2a6c69a6a97efeb182a066
PseudoDistant <pseudodistant@proton.me>
2023-12-31 21:00:54 -0500

    Added funne name things

--- src/main/java/G.java
+++ src/main/java/G.java
@@ -1,18 +1,23 @@
 import net.fabricmc.loader.impl.game.GameProvider;
+import net.fabricmc.loader.impl.game.patch.GamePatch;
 import net.fabricmc.loader.impl.game.patch.GameTransformer;
 import net.fabricmc.loader.impl.launch.FabricLauncher;
 import net.fabricmc.loader.impl.metadata.BuiltinModMetadata;
 import net.fabricmc.loader.impl.util.Arguments;
+import org.objectweb.asm.tree.AbstractInsnNode;
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.LdcInsnNode;
+import org.objectweb.asm.tree.MethodNode;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class G implements GameProvider {
-	private static final GameTransformer TRANSFORMER = new GameTransformer(
-	);
+	private static final GameTransformer T = new GameTransformer(new P());
 
 	@Override
 	public String getGameId() {
@@ -65,29 +70,29 @@ public class G implements GameProvider {
 	}
 
 	@Override
-	public boolean locateGame(FabricLauncher launcher, String[] args) {
+	public boolean locateGame(FabricLauncher a, String[] b) {
 		return true;
 	}
 
 	@Override
-	public void initialize(FabricLauncher launcher) {
-		TRANSFORMER.locateEntrypoints(launcher, Collections.singletonList(Path.of("./MC4K.jar")));
+	public void initialize(FabricLauncher a) {
+		T.locateEntrypoints(a, Collections.singletonList(Path.of("./MC4K.jar")));
 	}
 
 	@Override
 	public GameTransformer getEntrypointTransformer() {
-		return TRANSFORMER;
+		return T;
 	}
 
 	@Override
-	public void unlockClassPath(FabricLauncher launcher) {
-		launcher.addToClassPath(Paths.get("./MC4K.jar"));
+	public void unlockClassPath(FabricLauncher a) {
+		a.addToClassPath(Paths.get("./MC4K.jar"));
 	}
 
 	@Override
-	public void launch(ClassLoader loader) {
-		try {loader.loadClass("Viewer").getMethod("main", String[].class).invoke(null, (Object) new String[0]);}
-		catch(Exception ignored) {}
+	public void launch(ClassLoader a) {
+		try {a.loadClass("Viewer").getMethod("main", String[].class).invoke(null, (Object) new String[0]);}
+		catch(Exception b) {}
 	}
 
 	@Override
@@ -96,7 +101,28 @@ public class G implements GameProvider {
 	}
 
 	@Override
-	public String[] getLaunchArguments(boolean sanitize) {
+	public String[] getLaunchArguments(boolean a) {
 		return new String[0];
+	}
+}
+
+class P extends GamePatch {
+	@Override
+	public void process(FabricLauncher a, Function<String, ClassNode> b, Consumer<ClassNode> c) {
+		ClassNode d = b.apply("Viewer");
+		for (MethodNode e : d.methods) {
+			if (e.name.equals("<init>")) {
+				for (AbstractInsnNode i : e.instructions) {
+					if (i instanceof LdcInsnNode f) {
+						if (f.cst instanceof String g) {
+							if (g.contains("M")) {
+								e.instructions.set(i, new LdcInsnNode(g + " - Fabric"));
+								c.accept(d);
+							}
+						}
+					}
+				}
+			}
+		}
 	}
 }
